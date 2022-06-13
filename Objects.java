d1e8da907105e3e969043f5dddab341823dda51e
Maryam-Saba <105993857+Maryam-Saba@users.noreply.github.com>
2022-06-13 23:59:42 +0500

    Add files via upload

---
+++ Objects.java
@@ -0,0 +1,23 @@
+class Cats
+{
+int numbers=10;
+String Colour="Blue";
+void cry()
+{
+System.out.println("Black cats are beautiful");
+}
+void Colour()
+{
+System.out.println(Colour);
+}
+}
+public class Objects
+
+{
+public	static 	void 	main(String ars[])
+{
+Cats sleep= new Cats();
+sleep.cry();
+sleep.Colour();
+}
+}
